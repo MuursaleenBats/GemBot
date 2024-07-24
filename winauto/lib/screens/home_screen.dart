09dcdcfa0c16a32602569ebcbccbda2636530013
MuursaleenBats <mursaleenbatatawala@gmail.com>
2024-07-24 14:15:33 +0530

    homescreen UI completed

--- winauto/lib/screens/home_screen.dart
+++ winauto/lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,22 +15,129 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double textFormFieldWidth = screenWidth * 0.75;
     return Scaffold(
       backgroundColor: Color(0xFF1E1E1E),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Hello",
-              style: TextStyle(
-                color: Color(0x90FFFFFF),
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () => showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text(
+                        "Guide",
+                      ),
+                      content: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "1. ",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Text(
+                            "2. ",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          child: const Text('Ok'),
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Color(0xFF1E1E1E)),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Color(0x95FFFFFF))),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                splashRadius: 20.0,
+                splashColor: Color(0xffffa500),
+                icon: Icon(
+                  Icons.info,
+                  color: Color(0x50FFFFFF),
+                ),
               ),
-            )
-          ],
-        ),
+            ],
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 20.0),
+                    child: Image.asset(
+                      'assets/logo.jpg',
+                      height: 100.0,
+                      width: 100.0,
+                    ),
+                  ),
+                  Text(
+                    "Welcome to WinAuto",
+                    style: TextStyle(
+                      color: Color(0x90FFFFFF),
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
+            child: Container(
+              width: textFormFieldWidth,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    borderSide: BorderSide(color: Color(0x90FFFFFF)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    borderSide: BorderSide(color: Color(0x90FFFFFF)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  prefixIcon: IconButton(
+                    onPressed: () => null,
+                    splashRadius: 20.0,
+                    icon: Icon(
+                      Icons.mic,
+                      color: Color(0x90FFFFFF),
+                    ),
+                  ),
+                  hintText: "Enter Prompt",
+                  hintStyle: TextStyle(color: Color(0x90FFFFFF)),
+                ),
+                cursorColor: Colors.white,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
